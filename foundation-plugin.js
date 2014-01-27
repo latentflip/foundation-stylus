744bbdc1e6e00acdbbd4e0dbc7a203c206263889
Philip Roberts <phil@andyet.net>
2014-01-27 11:41:12 +0000

    Add plugin script

---
+++ foundation-plugin.js
@@ -0,0 +1,29 @@
+var stylus = require('stylus'),
+    path = require('path');
+
+exports = module.exports = plugin;
+
+/**
+ * Library version.
+ */
+
+exports.version = require(path.join(__dirname, './package.json')).version;
+
+/**
+ * Stylus path.
+ */
+
+exports.path = path.join(__dirname, 'styl');
+
+/**
+ * Return the plugin callback for stylus.
+ *
+ * @return {Function}
+ * @api public
+ */
+
+function plugin() {
+  return function (style) {
+    style.include(exports.path);
+  };
+}
